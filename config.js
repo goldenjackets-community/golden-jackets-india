3e2221971955e84810575e87e31d4bd61914f83a
Ricardo Gulias <111292744+ricardogulias@users.noreply.github.com>
2026-06-09 18:00:39 -0300

    fix: remove Oscar email (Chile, not India)

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var GJ_CONFIG = {
   ADMIN_API: 'https://kqiq2bltjd.execute-api.us-east-1.amazonaws.com/admin',
-  ADMIN_EMAILS: ['rachapudivishnu9@gmail.com', 'gaviria.oscar@gmail.com', 'ricardo.gulias@goldenjacketsbrazil.com', 'erickmancz@gmail.com', 'wagnermazevedo@hotmail.com'],
+  ADMIN_EMAILS: ['rachapudivishnu9@gmail.com', 'ricardo.gulias@goldenjacketsbrazil.com', 'erickmancz@gmail.com', 'wagnermazevedo@hotmail.com'],
   CHAPTER: 'india'
 };
